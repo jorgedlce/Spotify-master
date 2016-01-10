1ad0907c856b22cb22e00113aa0337d0f40aff03
jorge <jorgeluisdc_07@hotmailc.om>
2016-01-09 22:52:09 -0500

    Initial commit

---
+++ app/src/main/java/music/spotify/MainActivity.java
@@ -0,0 +1,21 @@
+package music.spotify;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Onclick(View v){
+        Intent intent = new Intent(this,MyMusic.class);
+        startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/music/spotify/MyMusic.java
@@ -0,0 +1,13 @@
+package music.spotify;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyMusic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_music);
+    }
+}
